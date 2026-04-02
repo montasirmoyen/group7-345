53ba9a029cd542fcf03cd69029f766f7bd1b12bc
monty <montasirmoyen@gmail.com>
2026-04-01 22:16:47 -0400

    feat: enhance layout with Toaster and TooltipProvider components

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,21 @@ import { Geist, Geist_Mono, Instrument_Sans } from "next/font/google"
 
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
+import { Toaster } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
 import { cn } from "@/lib/utils";
 
-const instrumentSans = Instrument_Sans({subsets:['latin'],variable:'--font-sans'})
+const instrumentSans = Instrument_Sans({ subsets: ['latin'], variable: '--font-sans' })
 
 const fontMono = Geist_Mono({
   subsets: ["latin"],
   variable: "--font-mono",
 })
+
+export const metadata = {
+  title: "Job Application Tracker",
+  description: "An app to organize and manage your job applications efficiently.",
+}
 
 export default function RootLayout({
   children,
@@ -23,7 +30,12 @@ export default function RootLayout({
       className={cn("antialiased", fontMono.variable, "font-sans", instrumentSans.variable)}
     >
       <body>
-        <ThemeProvider>{children}</ThemeProvider>
+        <ThemeProvider>
+          <TooltipProvider>
+            {children}
+            <Toaster />
+          </TooltipProvider>
+        </ThemeProvider>
       </body>
     </html>
   )
